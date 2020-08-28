cc2546ca90c051dc7a9709b794199349c66adaf0
Lenovo <anderson6466@hotmail.com>
2020-08-28 13:10:35 -0500

    Archivos

---
+++ BD/ScriptBD.sql
@@ -0,0 +1,10 @@
+
+
+create database DEMO3 ;
+use DEMO3;
+create  table Log_Values (
+
+  mensaje  varchar(1000),
+  codigoerror varchar(1000)
+
+)
---
+++ Examen/src/Examen/Pregunta8.java
@@ -0,0 +1,22 @@
+package Examen;
+import java.util.HashMap;
+import java.util.Map;
+
+import Examen.Conexion; 
+public class Pregunta8 {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		
+		
+		 
+		 		Conexion.LogMessage("Esto es un error", true, false, false);
+		
+		
+		// TODO Auto-generated method stub
+System.out.print("hola a todos");
+	}
+
+}
